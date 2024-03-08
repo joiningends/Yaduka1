576800001cac51bc094f775dd988f167c7f702f7
Abhi-shubu <115418689+Abhi-shubu@users.noreply.github.com>
2024-03-09 04:53:25 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 const cors = require("cors");
 const sequelize = require("./util/database");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +16,8 @@ app.options("*", cors());
 // Middleware
 app.use(express.json());
 app.use(morgan("tiny"));
+
+
 
 //Routes
 
@@ -31,32 +33,36 @@ const productRoutes = require("./routes/productRoutes");
 const unitRoutes = require("./routes/unitRoutes");
 const spaceTypesRoutes = require("./routes/spacetypes");
 const underRoutes = require("./routes/unders");
-const locationRoutes = require("./routes/locations");
-const gstrateRoutes = require("./routes/gstrates");
-const gsttypeRoutes = require("./routes/gsttypes");
-const contractsRoutes = require("./routes/contracts");
-const requisitionRoutes = require("./routes/requisitiof");
-const allinvRoutes = require("./routes/allinvoice");
+const locationRoutes = require("./routes/locations")
+ const gstrateRoutes = require("./routes/gstrates")
+ const gsttypeRoutes = require("./routes/gsttypes") 
+ const contractsRoutes = require("./routes/contracts")
+const requisitionRoutes = require("./routes/requisitiof")
+const allinvRoutes = require("./routes/allinvoice")
+
+
+
 
 const api = process.env.API_URL;
-app.use(`${api}/users`, usersRoutes);
-app.use(`${api}/commodityType`, commodityTypeRoutes);
-app.use(`${api}/commodity`, commodityRoutes);
-
-app.use(`${api}/varient`, varientRoutes);
-
-app.use(`${api}/size`, sizeDataRoutes);
-app.use(`${api}/quality`, qualityRoutes);
-app.use(`${api}/product`, productRoutes);
-app.use(`${api}/unit`, unitRoutes);
-app.use(`${api}/spacetype`, spaceTypesRoutes);
-app.use(`${api}/under`, underRoutes);
-app.use(`${api}/location`, locationRoutes);
-app.use(`${api}/gstrates`, gstrateRoutes);
-app.use(`${api}/gsttypes`, gsttypeRoutes);
-app.use(`${api}/contracts`, contractsRoutes);
-app.use(`${api}/ref`, requisitionRoutes);
-app.use(`${api}/allinvoice`, allinvRoutes);
+  app.use(`${api}/users`, usersRoutes);
+  app.use(`${api}/commodityType`, commodityTypeRoutes);
+  app.use(`${api}/commodity`, commodityRoutes);
+ 
+  app.use(`${api}/varient`, varientRoutes);
+  
+  
+  app.use(`${api}/size`, sizeDataRoutes);
+  app.use(`${api}/quality`, qualityRoutes);
+  app.use(`${api}/product`, productRoutes);
+  app.use(`${api}/unit`, unitRoutes);
+  app.use(`${api}/spacetype`, spaceTypesRoutes)
+  app.use(`${api}/under`, underRoutes)
+  app.use(`${api}/location`, locationRoutes)
+  app.use(`${api}/gstrates`, gstrateRoutes)
+app.use(`${api}/gsttypes`, gsttypeRoutes)
+app.use(`${api}/contracts`, contractsRoutes)
+app.use(`${api}/ref`, requisitionRoutes)
+app.use(`${api}/allinvoice`, allinvRoutes)
 
 const userTable = require("./models/user");
 const UserUnder = require("./models/userunder");
@@ -64,29 +70,31 @@ const UserUnder = require("./models/userunder");
 const commodityType = require("./models/commodityType");
 const commodity = require("./models/commodity");
 
+
 const varient = require("./models/varient");
+
 
 const size = require("./models/size");
 const quality = require("./models/quality");
 const product = require("./models/product");
 const unit = require("./models/unit");
-const SpaceType = require("./models/spacetype");
+const SpaceType = require('./models/spacetype');
 
-const Location = require("./models/location");
+const  Location = require("./models/location");
 const SpaceDetails = require("./models/SpaceDetails");
 const Under = require("./models/under");
 const Contract = require("./models/contract");
 const GstType = require("./models/gsttype");
-const GstRate = require("./models/gstrate");
+const  GstRate  = require('./models/gstrate');
 const ContractSpace = require("./models/contractspace");
 const ContractProduct = require("./models/contractproduct");
 const Invoice = require("./models/invoice");
 const Requisition = require("./models/requisition");
 const Reproduct = require("./models/reproduct");
 const party = require("./models/party");
-const Address = require("./models/address");
-const BankDetails = require("./models/bankdetails");
-const Signature = require("./models/signeture");
+const Address = require('./models/address');
+const BankDetails = require('./models/bankdetails');
+const Signature = require('./models/signeture');
 commodity.belongsTo(commodityType);
 
 //varient
@@ -105,81 +113,45 @@ product.belongsTo(unit);
 product.belongsTo(commodity);
 // In UserUnder model
 UserUnder.belongsTo(userTable, { foreignKey: "partyid", as: "party" });
-UserUnder.belongsTo(userTable, {
-  foreignKey: "underValues",
-  as: "valueofunder",
-});
+UserUnder.belongsTo(userTable, { foreignKey: "underValues", as: "valueofunder" });
 
-SpaceDetails.belongsTo(SpaceType, { foreignKey: "type" });
-Location.hasMany(SpaceDetails, {
-  as: "spaceDetails",
-  foreignKey: "locationId",
-});
-SpaceDetails.belongsTo(Location, { foreignKey: "locationId" });
-SpaceDetails.belongsTo(Under, { foreignKey: "under", as: "undertype" });
+SpaceDetails.belongsTo(SpaceType, { foreignKey: 'type' });
+ Location.hasMany(SpaceDetails, { as: 'spaceDetails', foreignKey: 'locationId' });
+ SpaceDetails.belongsTo(Location, { foreignKey: 'locationId' });
+SpaceDetails.belongsTo(Under, { foreignKey: 'under', as: "undertype" });
 
-Contract.belongsTo(userTable, { foreignKey: "under", as: "underadmin" });
-Contract.belongsTo(Location, { foreignKey: "storageId", as: "location" });
-Contract.belongsTo(GstRate, {
-  foreignKey: "gstrate",
-  as: "gstRate",
-  allowNull: true,
-});
-Contract.belongsTo(GstType, {
-  foreignKey: "gsttype",
-  as: "gstType",
-  allowNull: true,
-});
+Contract.belongsTo(userTable, { foreignKey: 'under', as: 'underadmin' });
+Contract.belongsTo(Location, { foreignKey: 'storageId', as: 'location' });
+Contract.belongsTo(GstRate, { foreignKey: 'gstrate', as: 'gstRate', allowNull: true }); 
+Contract.belongsTo(GstType, { foreignKey: 'gsttype', as: 'gstType', allowNull: true });
 //Contract.belongsTo(userTable, { foreignKey: 'partyId', as: 'user' });
-Contract.belongsTo(userTable, {
-  foreignKey: "partyId",
-  as: "partyuser",
-  allowNull: true,
-});
-Contract.belongsTo(party, {
-  foreignKey: "partyidinpartytable",
-  as: "partyus",
-  allowNull: true,
-});
-Contract.hasMany(ContractSpace, { as: "spaces", foreignKey: "contractId" });
-ContractSpace.belongsTo(Contract, { foreignKey: "contractId" });
-ContractSpace.belongsTo(SpaceDetails, {
-  foreignKey: "storagespace",
-  as: "storagespaces",
-});
-Contract.hasMany(ContractProduct, { as: "space", foreignKey: "contractId" });
-ContractProduct.belongsTo(Contract, { as: "space", foreignKey: "contractId" });
-ContractProduct.belongsTo(SpaceDetails, {
-  foreignKey: "storagespace",
-  as: "storagespac",
-});
-ContractProduct.belongsTo(product, { foreignKey: "productid", as: "product" });
-Contract.hasMany(Invoice, { as: "invoice", foreignKey: "contractId" });
-userTable.hasMany(Invoice, { as: "admin", foreignKey: "userId" });
+Contract.belongsTo(userTable, { foreignKey: 'partyId', as: 'partyuser', allowNull: true });
+Contract.belongsTo(party, { foreignKey: 'partyidinpartytable', as: 'partyus',allowNull: true });
+Contract.hasMany(ContractSpace, { as: 'spaces', foreignKey: 'contractId' });
+ContractSpace.belongsTo(Contract, { foreignKey: 'contractId' });
+ContractSpace.belongsTo(SpaceDetails, { foreignKey: 'storagespace', as: 'storagespaces' });
+Contract.hasMany(ContractProduct, { as: 'space', foreignKey: 'contractId' });
+ContractProduct.belongsTo(Contract, { as: 'space', foreignKey: 'contractId' });
+ContractProduct.belongsTo(SpaceDetails, { foreignKey: 'storagespace', as: 'storagespac' });
+ContractProduct.belongsTo(product, { foreignKey: 'productid', as: 'product' });
+Contract.hasMany(Invoice, { as: 'invoice', foreignKey: 'contractId' });
+userTable.hasMany(Invoice, { as: 'admin', foreignKey: 'userId' });
 
 //Requisition.belongsTo(userTable, { foreignKey: 'partyId', as: 'partyuser', allowNull: true });
 //Requisition.belongsTo(party, { foreignKey: 'partyidinpartytable', as: 'partyus',allowNull: true });
 Requisition.belongsTo(userTable, { foreignKey: "partyid", as: "part" });
-Requisition.belongsTo(Contract, { foreignKey: "contractId", as: "conf" });
-Requisition.belongsTo(Location, { foreignKey: "storageId", as: "locatio" });
-Reproduct.belongsTo(ContractProduct, {
-  foreignKey: "contractproductid",
-  as: "contractproduct",
-});
-Requisition.hasMany(Reproduct, { as: "reproducts", foreignKey: "requationId" });
-Requisition.belongsTo(userTable, {
-  foreignKey: "underValues",
-  as: "valueofunde",
-});
+Requisition.belongsTo(Contract, { foreignKey: 'contractId',as: "conf" });
+Requisition.belongsTo(Location, { foreignKey: 'storageId', as: 'locatio' });
+Reproduct.belongsTo(ContractProduct, { foreignKey: 'contractproductid', as: 'contractproduct' });
+Requisition.hasMany(Reproduct, { as: 'reproducts', foreignKey: 'requationId' });
+Requisition.belongsTo(userTable, { foreignKey: "underValues", as: "valueofunde" });
 
-Reproduct.belongsTo(Requisition, {
-  as: "requtaion",
-  foreignKey: "requationId",
-});
-sequelize
-  .sync()
+Reproduct.belongsTo(Requisition, { as: 'requtaion', foreignKey: 'requationId' });
+sequelize.sync()
   .then(() => {
-    console.log("Database synchronization complete.");
+    console.log('Database synchronization complete.');
+
+  
 
     // Start the server
     const PORT = process.env.PORT || 5001;
@@ -187,6 +159,7 @@ sequelize
       console.log(`Server is running at http://localhost:${PORT}`);
     });
   })
-  .catch(err => {
-    console.error("Error syncing the database:", err);
+  .catch((err) => {
+    console.error('Error syncing the database:', err);
   });
+  
