ad671fd4346a90f6b3eeb0dcd4014e6c7cd92c2d
Ashutosh Bisht <81284766+Ashutoshbishtt@users.noreply.github.com>
2024-04-16 16:48:15 +0530

    Update contractController.js

--- server/Controllers/contractController.js
+++ server/Controllers/contractController.js
@@ -1604,7 +1604,7 @@ const generatePDF = async (userId, existingContract, tableData) => {
     });
 
     // Construct the media URL for WhatsApp
-    const media_url = `https://appointments.inspirononline.com/${filePath}`;
+    const media_url = `http://3.6.248.144/${filePath}`;
 
     // Send WhatsApp message with the PDF attachment
     sendWhatsAppMessageMedia(
