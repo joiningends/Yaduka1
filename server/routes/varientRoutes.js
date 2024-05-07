6467e4c5bbd1307f19dddb0b2689857c4481c651
Ashutosh Bisht <ashu9872180@gmail.com>
2024-05-07 16:36:06 +0530

    Changes

--- server/routes/varientRoutes.js
+++ server/routes/varientRoutes.js
@@ -23,7 +23,7 @@ const s3 = new aws.S3();
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: "inspirionimages",
+    bucket: "yasukaimages",
     contentType: multerS3.AUTO_CONTENT_TYPE,
 
     key: function (req, file, cb) {
